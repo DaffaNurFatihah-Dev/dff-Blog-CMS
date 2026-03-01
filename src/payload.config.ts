d99147932bc2e837a57859a4f25cb0281bd2348b
DaffaNurFatihah-Dev <daffanurfatihahh@gmail.com>
2026-03-01 16:09:19 +0700

    feat: first commit

---
+++ src/payload.config.ts
@@ -0,0 +1,46 @@
+// storage-adapter-import-placeholder
+import { postgresAdapter } from '@payloadcms/db-postgres'
+import { FixedToolbarFeature, lexicalEditor } from '@payloadcms/richtext-lexical'
+import path from 'path'
+import { buildConfig } from 'payload'
+import { fileURLToPath } from 'url'
+import sharp from 'sharp'
+
+import { Users } from './collections/Users'
+import { Media } from './collections/Media/config'
+import { env } from './lib/env'
+import { Articles } from './collections/Articles/config'
+import { ArticleAuthors } from './collections/ArticleAuthors/config'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
+
+export default buildConfig({
+    admin: {
+        user: Users.slug,
+        importMap: {
+            baseDir: path.resolve(dirname),
+        },
+        autoLogin: {
+            email: env.CMS_SEED_ADMIN_EMAIL,
+            password: env.CMS_SEED_ADMIN_PASSWORD,
+        },
+    },
+    collections: [Users, Media, Articles, ArticleAuthors],
+    editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [...defaultFeatures, FixedToolbarFeature()],
+    }),
+    secret: process.env.PAYLOAD_SECRET || '',
+    typescript: {
+        outputFile: path.resolve(dirname, 'payload-types.ts'),
+    },
+    db: postgresAdapter({
+        pool: {
+            connectionString: process.env.DATABASE_URI || '',
+        },
+    }),
+    sharp,
+    plugins: [
+        // storage-adapter-placeholder
+    ],
+})
